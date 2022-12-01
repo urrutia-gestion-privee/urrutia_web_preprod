2a5883016fab974e1785d98fae65672eacb4319c
vivien.haese <vivien.haese@gmail.com>
2022-12-01 23:03:19 +0100

    update cname

--- deploy-preprod.sh
+++ deploy-preprod.sh
@@ -3,9 +3,5 @@
 # abort on errors
 set -e
 
-# if you are deploying to a custom domain
-echo 'preprod.urrutia.fr' > CNAME
-
 git push -f git@github.com:urrutia-gestion-privee/urrutia_web_preprod.git preprod
 
-rm CNAME
