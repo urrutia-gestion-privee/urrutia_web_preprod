60e0b219cdaed35f2ef8f61dcd4ed722c2ff986f
vivien.haese <vivien.haese@gmail.com>
2022-12-19 21:43:36 +0100

    Modifications après points

--- js/functions.js
+++ js/functions.js
@@ -541,6 +541,37 @@ jQuery($=> {
         });
     }
 
+    let morphRotatingTextCitation = $("#morph-text-citation");
+    if (morphRotatingTextCitation.length) {
+        morphRotatingTextCitation.Morphext({
+            // The [in] animation type. Refer to Animate.css for a list of available animations.
+            animation: "flipInX",
+            // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+            separator: "|",
+            // The delay between the changing of each phrase in milliseconds.
+            speed: 6000,
+            complete: function () {
+                // Called after the entrance animation is executed.
+            }
+        });
+    }
+
+    let morphRotatingTextAuthor = $("#morph-text-author");
+    if (morphRotatingTextAuthor.length) {
+        morphRotatingTextAuthor.Morphext({
+            // The [in] animation type. Refer to Animate.css for a list of available animations.
+            animation: "flipInX",
+            // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+            separator: "|",
+            // The delay between the changing of each phrase in milliseconds.
+            speed: 6000,
+            complete: function () {
+                // Called after the entrance animation is executed.
+            }
+        });
+    }
+    
+
     /* =====================================
      Parallax And responsive plugins initialize
       ====================================== */
